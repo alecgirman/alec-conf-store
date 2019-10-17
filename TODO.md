d932ab8a0904ab09421cddde69cff5cb527a38cf
Alec Girman <avgch1@gmail.com>
2019-10-17 20:00:34 +0000

    improved efficiency and added GPU drivers

--- TODO.md
+++ TODO.md
@@ -13,3 +13,11 @@ Long term goals/ideas:
 
 [ ] Make ashe a custom shell written in C/C++
 [ ] Maybe add some sort of vim keybind functionality to make it great for power users
+
+
+
+Misc:
+
+[ ] Make boot script clone and backup the ashe directory on boot
+[ ] shutdown script using alias auto-push to unstable/nightly
+
--- zsh/boot/core.zsh
+++ zsh/boot/core.zsh
@@ -18,26 +18,23 @@ pacman-no-confirm() {
 
 # TODO: modularize these
 # auto installs - no confirmation needed
-pacman-no-confirm neofetch
+pacman-no-confirm nvidia
+pacman-no-confirm neofetch htop lynx
 pacman-no-confirm neovim neovim-qt
-pacman-no-confirm htop
-pacman-no-confirm binutils
-pacman-no-confirm gcc
-pacman-no-confirm python3 python3-pip
-pacman-no-confirm lynx
-pacman-no-confirm xorg-server
+pacman-no-confirm binutils gcc
+pacman-no-confirm python3 python-pip ipython
+pacman-no-confirm plasma kde-applications qt5
+pacman-no-confirm xorg-server 
 pacman-no-confirm xorg-xinit
-pacman-no-confirm konsole dolphin chromium
-pacman-no-confirm qt5
-pacman-no-confirm kde-applications
-pacman-no-confirm plasma
+pacman-no-confirm konsole dolphin 
+pacman-no-confirm chromium
 
 echo "source ~/ashe/nvim/init.vim" > ~/.config/nvim/init.vim
 
 #pacman-no-confirm docker
 
 echo 'Updating persistent cache...'
-cp /var/cache/pacman/pkg/* /fdp/pkgcache/ -u
+cp /var/cache/pacman/pkg/* /fdp/pkgcache/ -un
 echo 'Complete!'
 echo "exec startplasma-x11" > /root/.xinitrc
 echo "starting xinit, please hold..."
