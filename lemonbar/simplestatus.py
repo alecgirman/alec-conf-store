da57ce64a44d19e9ee9f3d94dc6a9e3b599dfea5
Alec <avgch1@gmail.com>
2020-01-01 16:12:45 -0500

    Added lemonbar files WIP

--- lemonbar/simplestatus.py
+++ lemonbar/simplestatus.py
@@ -4,8 +4,14 @@ import sys, time
 
 def main():
     n = 0
+
+    # works in dzen2 and lemonbar...
+    # TODO: see if polybar handles ipc like this
     while True:
         print('Hello World ' + str(n))
+
+        # VERY IMPORTANT DO NOT REMOVE THIS LINE
+        #     - MANAGEMENT
         sys.stdout.flush()
         n = n + 1
         time.sleep(1)
